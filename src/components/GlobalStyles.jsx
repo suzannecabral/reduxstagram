586979c6f9cdd4e11d478ef0e19f240ce84cdcf8
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2021-09-29 14:21:20 -0700

    normalize css

--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Global, css } from '@emotion/react';
+import emotionNormalize from 'emotion-normalize';
 
 const GlobalStyles = (props) => {
 return (<div>
     <Global styles={css`
+        ${emotionNormalize}
+      `}/>
+    <Global styles={css`
+            @import url('http://fonts.cdnfonts.com/css/billabong');
         h1{
           color: hotpink;
+          font-family: 'Billabong', sans-serif;
+          font-size:60px;
         }
-      `}/>
+    `}/>
   </div>
 )};
 
